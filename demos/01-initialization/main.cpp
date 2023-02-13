6cbcccb35d248cf6ae56330507bd95147a956a0e
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2023-02-13 23:55:36 +0100

    demos: Reduce initialization demo to the intended scope

--- demos/01-initialization/main.cpp
+++ demos/01-initialization/main.cpp
@@ -1,33 +1,11 @@
-#include <fstream>
-#include <iostream>
-#include <chrono>
-
 #include "../../src/gfx/gfx.h"
-#include "../../src/gfx/gfx_spirv.h"
 
-#include "../../src/util/util_bitstream.h"
 #include "../../src/util/util_error.h"
-#include "../../src/util/util_huffman.h"
 #include "../../src/util/util_log.h"
 
 #include "../../src/wsi/wsi.h"
 
 using namespace as;
-
-struct UboData {
-  float color[4];
-  int size[2];
-  int dummy[2];
-};
-
-std::vector<char> loadFile(const char* path) {
-  std::ifstream file(path, std::ios::binary);
-  std::vector<char> data;
-  char ch = '\0';
-  while (file.get(ch))
-    data.push_back(ch);
-  return data;
-}
 
 void run_app() {
   Wsi wsi(WsiBackend::eDefault);
@@ -37,13 +15,11 @@ void run_app() {
     GfxInstanceFlag::eDebugMarkers |
     GfxInstanceFlag::eApiValidation);
 
-  WsiWindowDesc desc;
-  desc.title = "Window title";
-  desc.surfaceType = gfx->getBackendType();
+  WsiWindowDesc windowDesc;
+  windowDesc.title = "Initialization";
+  windowDesc.surfaceType = gfx->getBackendType();
 
-  WsiWindow window = wsi->createWindow(desc);
-  window->setKeyboardMode(WsiKeyboardMode::eText);
-
+  WsiWindow window = wsi->createWindow(windowDesc);
   GfxDevice device = gfx->createDevice(gfx->enumAdapters(0));
 
   GfxPresenterDesc presenterDesc;
@@ -53,77 +29,6 @@ void run_app() {
 
   GfxPresenter presenter = device->createPresenter(presenterDesc);
 
-  GfxShaderBinaryDesc binaryDesc;
-  binaryDesc.format = GfxShaderFormat::eVulkanSpirv;
-  binaryDesc.data = loadFile("/home/philip/vert.spv");
-  GfxShaderDesc shaderDesc = *reflectSpirvBinary(binaryDesc.data.size(), binaryDesc.data.data());
-  GfxShader vs(std::move(shaderDesc), std::move(binaryDesc));
-
-  binaryDesc.format = GfxShaderFormat::eVulkanSpirv;
-  binaryDesc.data = loadFile("/home/philip/frag.spv");
-  shaderDesc = *reflectSpirvBinary(binaryDesc.data.size(), binaryDesc.data.data());
-  GfxShader fs(std::move(shaderDesc), std::move(binaryDesc));
-
-  GfxGraphicsPipelineDesc graphicsDesc;
-  graphicsDesc.vertex = vs;
-  graphicsDesc.fragment = fs;
-
-  GfxGraphicsPipeline graphics = device->createGraphicsPipeline(graphicsDesc);
-
-  GfxVertexInputStateDesc vertexDesc;
-  vertexDesc.primitiveTopology = GfxPrimitiveType::eTriangleList;
-  vertexDesc.attributes[0].binding = 0;
-  vertexDesc.attributes[0].format = GfxFormat::eR32G32f;
-  vertexDesc.attributes[0].offset = 0;
-  vertexDesc.attributes[0].inputRate = GfxInputRate::ePerVertex;
-
-  GfxVertexInputState vertexState = device->createVertexInputState(vertexDesc);
-
-  std::array<float, 6> vertexData = {{
-    0.3f, 0.3f,
-    0.7f, 0.3f,
-    0.5f, 0.7f,
-  }};
-
-  std::array<uint32_t, 3> indexData = {{
-    0, 1, 2,
-  }};
-
-  GfxBufferDesc bufferDesc;
-  bufferDesc.debugName = "Vertex buffer";
-  bufferDesc.usage = GfxUsage::eVertexBuffer | GfxUsage::eCpuWrite;
-  bufferDesc.size = sizeof(vertexData);
-
-  GfxBuffer vertexBuffer = device->createBuffer(bufferDesc, GfxMemoryType::eAny);
-  std::memcpy(vertexBuffer->map(GfxUsage::eCpuWrite, 0), vertexData.data(), sizeof(vertexData));
-
-  bufferDesc.debugName = "Index buffer";
-  bufferDesc.usage = GfxUsage::eIndexBuffer | GfxUsage::eCpuWrite;
-  bufferDesc.size = sizeof(indexData);
-
-  GfxBuffer indexBuffer = device->createBuffer(bufferDesc, GfxMemoryType::eAny);
-  std::memcpy(indexBuffer->map(GfxUsage::eCpuWrite, 0), indexData.data(), sizeof(indexData));
-
-  GfxDescriptorArrayDesc samplerArrayDesc;
-  samplerArrayDesc.debugName = "Samplers";
-  samplerArrayDesc.bindingType = GfxShaderBindingType::eSampler;
-  samplerArrayDesc.descriptorCount = 256;
-
-  GfxDescriptorArray samplerArray = device->createDescriptorArray(samplerArrayDesc);
-
-  GfxDescriptorArrayDesc textureArrayDesc;
-  textureArrayDesc.debugName = "Textures";
-  textureArrayDesc.bindingType = GfxShaderBindingType::eResourceImageView;
-  textureArrayDesc.descriptorCount = 65536;
-
-  GfxDescriptorArray textureArray = device->createDescriptorArray(textureArrayDesc);
-
-  GfxSamplerDesc samplerDesc;
-  samplerDesc.debugName = "hello";
-
-  GfxSampler sampler = device->createSampler(samplerDesc);
-  samplerArray->setDescriptor(0, sampler->getDescriptor());
-
   bool quit = false;
 
   while (!quit) {
@@ -132,7 +37,7 @@ void run_app() {
            || e.type == WsiEventType::eWindowClose;
     });
 
-    presenter->present([graphics, vertexState, vertexBuffer, indexBuffer, samplerArray, textureArray] (const GfxPresenterContext& args) {
+    presenter->present([] (const GfxPresenterContext& args) {
       GfxContext context = args.getContext();
       GfxImage image = args.getImage();
 
@@ -147,29 +52,14 @@ void run_app() {
       viewDesc.subresource = image->getAvailableSubresources();
       viewDesc.usage = GfxUsage::eRenderTarget;
 
+      GfxImageView view = image->createView(viewDesc);
+
       GfxRenderingInfo renderInfo;
       renderInfo.color[0].op = GfxRenderTargetOp::eClear;
-      renderInfo.color[0].view = image->createView(viewDesc);
+      renderInfo.color[0].view = view;
       renderInfo.color[0].clearValue = GfxColorValue(1.0f, 1.0f, 1.0f, 1.0f);
 
-      Extent2D imageExtent = Extent2D(image->getDesc().extent);
-
-      GfxViewport viewport;
-      viewport.extent = Vector2D(imageExtent);
-      viewport.scissor.extent = imageExtent;
-
       context->beginRendering(renderInfo, 0);
-      context->bindPipeline(graphics);
-      context->bindDescriptorArray(0, samplerArray);
-      context->bindDescriptorArray(1, textureArray);
-      context->bindIndexBuffer(indexBuffer->getDescriptor(
-        GfxUsage::eIndexBuffer, 0, indexBuffer->getDesc().size),
-        GfxFormat::eR32ui);
-      context->bindVertexBuffer(0, vertexBuffer->getDescriptor(
-        GfxUsage::eVertexBuffer, 0, vertexBuffer->getDesc().size), 8);
-      context->setVertexInputState(vertexState);
-      context->setViewport(viewport);
-      context->drawIndexed(3, 1, 0, 0, 0);
       context->endRendering();
 
       // Prepare the swap chain image for presentation
