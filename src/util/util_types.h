3aaba74893a4981b90612d0c7f62e313ca084994
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2023-02-18 16:32:46 +0100

    util: Add matrix type.

--- src/util/util_types.h
+++ src/util/util_types.h
@@ -23,6 +23,10 @@ class Vector {
 
 public:
 
+  static constexpr size_t Components = N;
+
+  using ScalarType = T;
+
   Vector() = default;
 
   /**
@@ -443,9 +447,224 @@ private:
 };
 
 
+/**
+ * \brief Matrix
+ *
+ * Simple matrix type. Much like the \ref Vector class, this is
+ * \e not aimed to provide high performance, and only provides
+ * very basic functionality. The data layout is column-major to
+ * match graphics APIs.
+ * \tparam T Scalar type
+ * \tparam N Row count
+ * \tparam M Column count
+ */
+template<typename T, size_t N, size_t M>
+class Matrix {
+
+public:
+
+  static constexpr size_t Rows = N;
+  static constexpr size_t Cols = M;
+
+  using ScalarType = T;
+  using VectorType = Vector<T, N>;
+
+  Matrix() = default;
+
+  /**
+   * \brief Initializes matrix
+   *
+   * Initializes matrix with the given
+   * vector arguments.
+   * \param [in] args Arguments
+   */
+  template<typename... Tx>
+  explicit Matrix(const Tx&... args) {
+    init(std::integral_constant<size_t, 0>(), args...);
+  }
+
+  /**
+   * \brief Matrix scaling
+   *
+   * \param [in] factor Source operand
+   * \returns Reference to self
+   */
+  Matrix& operator *= (T factor) {
+    for (size_t c = 0; c < Cols; c++) {
+      for (size_t r = 0; r < Rows; r++)
+        m_cols[c][r] *= factor;
+    }
+
+    return *this;
+  }
+
+  /**
+   * \brief Matrix product
+   *
+   * This only works for square matrices and
+   * only serves as a convenience method.
+   * \param [in] matrix Source operand
+   * \returns Reference to self
+   */
+  template<std::enable_if_t<Rows == Cols, bool> = true>
+  Matrix& operator *= (const Matrix& matrix) {
+    return (*this = ((*this) * matrix));
+  }
+
+  /**
+   * \brief Matrix scaling
+   *
+   * \param [in] factor Source operand
+   * \returns Resulting matrix
+   */
+  Matrix operator * (T factor) {
+    Matrix result;
+
+    for (size_t c = 0; c < Cols; c++) {
+      for (size_t r = 0; r < Rows; r++)
+        result.m_cols[c][r] = m_cols[c][r] * factor;
+    }
+
+    return result;
+  }
+
+  /**
+   * \brief Matrix product
+   *
+   * \param [in] matrix Source operand
+   * \returns Resulting matrix
+   */
+  template<size_t M_>
+  Matrix<T, N, M_> operator * (const Matrix<T, M, M_>& matrix) const {
+    Matrix<T, N, M_> result;
+
+    for (size_t c = 0; c < M_; c++) {
+      for (size_t r = 0; r < N; r++) {
+        T sum = T(0);
+
+        for (size_t v = 0; v < Cols; v++)
+          sum += m_cols[v][r] * matrix.m_cols[c][v];
+
+        result.m_cols[c][r] = sum;
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * \brief Retrieves given column
+   *
+   * \param [in] idx Column index
+   * \returns Reference to given column vector
+   */
+  const Vector<T, N>& operator [] (uint32_t idx) const {
+    return m_cols[idx];
+  }
+
+  /**
+   * \brief Retrieves given column for writing
+   *
+   * \param [in] idx Column index
+   * \returns Reference to given column vector
+   */
+  Vector<T, N>& operator [] (uint32_t idx) {
+    return m_cols[idx];
+  }
+
+  /**
+   * \brief Retrieves column with compile-time index
+   *
+   * \tparam J Row index
+   * \tparam I Column index
+   * \returns Reference to given column vector
+   */
+  template<size_t I, std::enable_if_t<I < Cols, bool> = true>
+  const VectorType& at() const {
+    return m_cols[I];
+  }
+
+  /**
+   * \brief Retrieves column with compile-time index for writing
+   *
+   * \tparam I Column index
+   * \returns Reference to given column vector
+   */
+  template<size_t I, std::enable_if_t<I < Cols, bool> = true>
+  VectorType& at() {
+    return m_cols[I];
+  }
+
+  /**
+   * \brief Retrieves element with compile-time index
+   *
+   * \tparam J Row index
+   * \tparam I Column index
+   * \returns Reference to given element
+   */
+  template<size_t J, size_t I, std::enable_if_t<J < Rows && I < Cols, bool> = true>
+  const T& at() const {
+    return m_cols[I].template at<J>();
+  }
+
+  /**
+   * \brief Retrieves element with compile-time index for writing
+   *
+   * \tparam J Row index
+   * \tparam I Column index
+   * \returns Reference to given element
+   */
+  template<size_t J, size_t I, std::enable_if_t<J < Rows && I < Cols, bool> = true>
+  T& at() {
+    return m_cols[I].template at<J>();
+  }
+
+  bool operator == (const Matrix& matrix) const = default;
+  bool operator != (const Matrix& matrix) const = default;
+
+  /**
+   * \brief Creates identity matrix
+   * \returns Matrix with diagonal set to 1
+   */
+  static Matrix identity() {
+    Matrix result;
+
+    for (size_t c = 0; c < Cols; c++) {
+      for (size_t r = 0; r < Rows; r++)
+        result[c][r] = c == r ? T(1) : T(0);
+    }
+
+    return result;
+  }
+
+private:
+
+  void init(std::integral_constant<size_t, M>) { }
+
+  template<size_t I, typename... Tx>
+  void init(std::integral_constant<size_t, I>, Vector<T, N> arg, const Tx&... args) {
+    at<I>() = arg;
+    init(std::integral_constant<size_t, I + 1>(), args...);
+  }
+
+  VectorType m_cols[M];
+
+};
+
+
 using Offset2D = Vector<int32_t, 2>;
+using Offset3D = Vector<int32_t, 3>;
+
 using Extent2D = Vector<uint32_t, 2>;
+using Extent3D = Vector<uint32_t, 3>;
+
 using Vector2D = Vector<float, 2>;
+using Vector3D = Vector<float, 3>;
+using Vector4D = Vector<float, 4>;
+
+using Matrix2x2 = Matrix<float, 2, 2>;
+using Matrix3x3 = Matrix<float, 3, 3>;
+using Matrix4x4 = Matrix<float, 4, 4>;
 
 /**
  * \brief Rectangle
@@ -465,10 +684,6 @@ struct Rect2D {
   bool operator != (const Rect2D&) const = default;
 };
 
-
-using Offset3D = Vector<int32_t, 3>;
-using Extent3D = Vector<uint32_t, 3>;
-using Vector3D = Vector<float, 3>;
 
 /**
  * \brief Box
