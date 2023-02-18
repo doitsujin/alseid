02e39bec5d59660a9b76c49a4502ea14de034a15
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2023-02-18 16:32:59 +0100

    util: Add convenient wrapper to use mutable lvalue references.

--- src/util/util_common.h
+++ src/util/util_common.h
@@ -2,3 +2,42 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <utility>
+
+namespace as {
+
+/**
+ * \brief Object wrapper for temporary lvalue references
+ *
+ * Useful to wrap mutable objects like streams
+ * in case some side effects can be discarded.
+ */
+template<typename T>
+class Lwrap {
+
+public:
+
+  template<typename... Args>
+  Lwrap(Args&&... args)
+  : m_object(std::forward<Args>(args)...) { }
+
+  operator T& () {
+    return m_object;
+  }
+
+private:
+
+  T m_object;
+
+};
+
+/**
+ * \brief Wraps object
+ * \returns Object
+ */
+template<typename T>
+auto lwrap(T&& object) {
+  return Lwrap<T>(std::move(object));
+}
+
+}
