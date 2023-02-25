013ca69de0cc44cd8b860da12b5bc9baae6d925a
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2023-02-25 17:07:27 +0100

    gfx: Add asynchronous transfer manager.

---
+++ src/gfx/gfx_transfer.cpp
@@ -0,0 +1,460 @@
+#include "../util/util_log.h"
+
+#include "gfx_transfer.h"
+
+namespace as {
+
+GfxTransferManagerIface::GfxTransferManagerIface(
+        Io                            io,
+        GfxDevice                     device,
+        uint64_t                      stagingBufferSize)
+: m_io                (std::move(io))
+, m_device            (std::move(device))
+, m_gpuDecompression  (m_device->getFeatures().gdeflateDecompression)
+, m_stagingAllocator  (stagingBufferSize) {
+  GfxBufferDesc bufferDesc;
+  bufferDesc.debugName = "GfxTransferManager staging buffer";
+  bufferDesc.usage = GfxUsage::eTransferSrc | GfxUsage::eCpuWrite | GfxUsage::eDecompressionSrc;
+  bufferDesc.size = stagingBufferSize;
+  bufferDesc.flags = GfxBufferFlag::eDedicatedAllocation;
+
+  m_stagingBuffer = m_device->createBuffer(bufferDesc, GfxMemoryType::eSystemMemory);
+
+  GfxSemaphoreDesc semaphoreDesc;
+  semaphoreDesc.debugName = "GfxTransferManager semaphore";
+
+  m_semaphore = m_device->createSemaphore(semaphoreDesc);
+
+  for (size_t i = 0; i < ContextCount; i++)
+    m_contexts[i] = m_device->createContext(GfxQueue::eComputeTransfer);
+
+  m_submissionThread = std::thread([this] { submit(); });
+  m_completionThread = std::thread([this] { retire(); });
+}
+
+
+GfxTransferManagerIface::~GfxTransferManagerIface() {
+  std::unique_lock lock(m_mutex);
+
+  flushLocked();
+
+  GfxTransferOp op;
+  op.type = GfxTransferOpType::eStop;
+
+  m_submissionQueue.push(std::move(op));
+  m_submissionCond.notify_one();
+
+  lock.unlock();
+
+  m_submissionThread.join();
+  m_completionThread.join();
+}
+
+
+uint64_t GfxTransferManagerIface::uploadBuffer(
+  const IoArchiveSubFile*             subFile,
+        GfxBuffer                     buffer,
+        uint64_t                      offset) {
+  std::unique_lock lock(m_mutex);
+
+  GfxTransferOp op;
+  op.type = GfxTransferOpType::eUploadBuffer;
+  op.subFile = subFile;
+  op.dstBuffer = std::move(buffer);
+  op.dstBufferOffset = offset;
+
+  return enqueueLocked(std::move(op));
+}
+
+
+uint64_t GfxTransferManagerIface::uploadImage(
+  const IoArchiveSubFile*             subFile,
+        GfxImage                      image,
+  const GfxImageSubresource&          subresources) {
+  std::unique_lock lock(m_mutex);
+
+  GfxTransferOp op;
+  op.type = GfxTransferOpType::eUploadImage;
+  op.subFile = subFile;
+  op.dstImage = std::move(image);
+  op.dstImageSubresources = subresources;
+
+  return enqueueLocked(std::move(op));
+}
+
+
+uint64_t GfxTransferManagerIface::flush() {
+  std::unique_lock lock(m_mutex);
+  return flushLocked();
+}
+
+
+uint64_t GfxTransferManagerIface::getCompletedBatchId() {
+  flush();
+
+  return m_semaphore->getCurrentValue();
+}
+
+
+void GfxTransferManagerIface::waitForCompletion(
+        uint64_t                      batch) {
+  return m_semaphore->wait(batch);
+}
+
+
+uint64_t GfxTransferManagerIface::flushLocked() {
+  if (!m_batchSize)
+    return m_batchId - 1;
+
+  GfxTransferOp op;
+  op.type = GfxTransferOpType::eFlush;
+  op.batchId = m_batchId;
+
+  m_submissionQueue.push(std::move(op));
+  m_submissionCond.notify_one();
+
+  m_batchSize = 0;
+  return m_batchId++;
+}
+
+
+uint64_t GfxTransferManagerIface::enqueueLocked(
+        GfxTransferOp&&               op) {
+  uint64_t alignedSize = computeAlignedSize(op.subFile);
+
+  // We can't allow any single batch to be larger than the
+  // staging buffer, so flush early if that's a problem.
+  if (m_batchSize + alignedSize > m_stagingAllocator.capacity())
+    flushLocked();
+
+  // Enqueue the operation. We do not have to wake up the
+  // worker thread since it won't do anything useful until
+  // the batch is flushed anyway.
+  uint64_t batchId = m_batchId;
+  op.batchId = batchId;
+
+  m_submissionQueue.push(std::move(op));
+
+  // Flush current batch if it uses at least a quarter of
+  // the staging buffer. This should help reduce stalls.
+  m_batchSize += alignedSize;
+
+  if (m_batchSize >= m_stagingAllocator.capacity() / 4)
+    flushLocked();
+
+  return batchId;
+}
+
+
+GfxContext GfxTransferManagerIface::acquireContext(
+        uint64_t                      batchId) {
+  if (batchId >= ContextCount)
+    m_semaphore->wait(batchId - ContextCount);
+
+  GfxContext context = m_contexts[batchId % ContextCount];
+  context->reset();
+
+  return context;
+}
+
+
+void GfxTransferManagerIface::submit() {
+  std::vector<GfxTransferOp> ops;
+
+  while (true) {
+    std::unique_lock lock(m_mutex);
+
+    m_submissionCond.wait(lock, [this] {
+      return !m_submissionQueue.empty();
+    });
+
+    // If the next operation in queue is an upload, just
+    // add it to the local buffer, otherwise flush.
+    while (!m_submissionQueue.empty()) {
+      GfxTransferOp op = std::move(m_submissionQueue.front());
+      m_submissionQueue.pop();
+
+      if (op.type == GfxTransferOpType::eStop) {
+        // Forward stop event to completion worker and exit
+        m_completionQueue.push(std::move(op));
+        m_completionCond.notify_one();
+        return;
+      }
+
+      if (op.type == GfxTransferOpType::eUploadBuffer || op.type == GfxTransferOpType::eUploadImage) {
+        ops.push_back(std::move(op));
+        continue;
+      }
+
+      if (op.type == GfxTransferOpType::eFlush) {
+        // Allocate staging memory in one go. Otherwise we'd get
+        // deadlocks if the allocator is too fragmented. Do not
+        // allocate staging memory for direct-upload buffers.
+        uint64_t stagingBufferSize = 0;
+        uint64_t stagingBufferOffset = 0;
+
+        for (auto& op : ops) {
+          if (!useDirectUpload(op)) {
+            op.stagingBufferOffset = stagingBufferSize;
+            op.stagingBufferSize = computeAlignedSize(op.subFile);
+
+            stagingBufferSize += op.stagingBufferSize;
+          }
+        }
+
+        // If necessary, wait for staging memory to get freed
+        m_retireCond.wait(lock, [this, stagingBufferSize, &stagingBufferOffset] {
+          auto offset = m_stagingAllocator.alloc(stagingBufferSize, 1);
+
+          if (!offset)
+            return false;
+
+          stagingBufferOffset = *offset;
+          return true;
+        });
+
+        lock.unlock();
+
+        // Build and submit the I/O request
+        IoRequest request = m_io->createRequest();
+
+        for (auto& op : ops) {
+          auto& archive = op.subFile->getArchive();
+          op.stagingBufferOffset += stagingBufferOffset;
+
+          if (useDirectUpload(op)) {
+            archive.read(request, op.subFile,
+              op.dstBuffer->map(GfxUsage::eCpuWrite, op.dstBufferOffset));
+          } else {
+            if (useGpuDecompression(op.subFile)) {
+              archive.readCompressed(request, op.subFile,
+                m_stagingBuffer->map(GfxUsage::eCpuWrite, op.stagingBufferOffset));
+            } else {
+              archive.read(request, op.subFile,
+                m_stagingBuffer->map(GfxUsage::eCpuWrite, op.stagingBufferOffset));
+            }
+          }
+        }
+
+        m_io->submit(request);
+
+        // Figure out how large the scratch buffer for image decompression needs
+        // to be, and recreate it with at least the required size if necessary.
+        uint64_t scratchBufferSize = 0;
+
+        for (auto& op : ops) {
+          if (op.type == GfxTransferOpType::eUploadImage && useGpuDecompression(op.subFile)) {
+            op.scratchBufferSize = align<uint64_t>(op.subFile->getSize(), 256);
+            scratchBufferSize = std::max(scratchBufferSize, op.scratchBufferSize);
+          }
+        }
+
+        if (scratchBufferSize && (!m_scratchBuffer || m_scratchBuffer->getDesc().size < scratchBufferSize)) {
+          GfxBufferDesc scratchDesc;
+          scratchDesc.debugName = "GfxTransferManager scratch buffer";
+          scratchDesc.usage = GfxUsage::eTransferSrc | GfxUsage::eDecompressionDst;
+          scratchDesc.size = std::max(1ull << findmsb(scratchBufferSize - 1), 16ull << 20);
+          scratchDesc.flags = GfxBufferFlag::eDedicatedAllocation;
+
+          m_scratchBuffer = m_device->createBuffer(scratchDesc, GfxMemoryType::eAny);
+        }
+
+        // Acquire a context for command recording
+        GfxContext context = acquireContext(op.batchId);
+
+        // Start with initializing all images to allow batching barriers.
+        for (const auto& op : ops) {
+          if (op.type == GfxTransferOpType::eUploadImage) {
+            context->imageBarrier(op.dstImage, op.dstImageSubresources,
+              0, 0, GfxUsage::eTransferDst, 0, GfxBarrierFlag::eDiscard);
+          }
+        }
+
+        // Record all buffer decompression and copy commands. These
+        // do not use scratch memory and can work independently.
+        for (const auto& op : ops) {
+          if (op.type != GfxTransferOpType::eUploadBuffer || useDirectUpload(op))
+            continue;
+
+          if (useGpuDecompression(op.subFile)) {
+            context->decompressBuffer(
+              op.dstBuffer, op.dstBufferOffset, op.subFile->getSize(),
+              m_stagingBuffer, op.stagingBufferOffset, op.subFile->getCompressedSize());
+          } else {
+            context->copyBuffer(op.dstBuffer, op.dstBufferOffset,
+              m_stagingBuffer, op.stagingBufferOffset, op.subFile->getSize());
+          }
+        }
+
+        // Record all image decompression and copy commands, but try to
+        // batch them as much as possible within the scratch buffer.
+        size_t firstCommand = 0;
+
+        while (firstCommand < ops.size()) {
+          size_t commandCount = 0;
+
+          uint64_t scratchSize = m_scratchBuffer ? m_scratchBuffer->getDesc().size : 0;
+          uint64_t scratchOffset = 0;
+
+          // If this is not the first set of commands using scratch memory,
+          // issue a barrier to prevent write-after-read hazards.
+          if (firstCommand) {
+            context->memoryBarrier(
+              GfxUsage::eTransferSrc, 0,
+              GfxUsage::eDecompressionDst, 0);
+          }
+
+          while (firstCommand + commandCount < ops.size()) {
+            auto& op = ops[firstCommand + commandCount];
+
+            if (op.type != GfxTransferOpType::eUploadImage || !useGpuDecompression(op.subFile)) {
+              commandCount += 1;
+              continue;
+            }
+
+            // If the scratch buffer is full, exit and record copy commands first.
+            if (scratchOffset + op.scratchBufferSize > scratchSize)
+              break;
+
+            // Otherwise, record the decompression command
+            op.scratchBufferOffset = scratchOffset;
+
+            context->decompressBuffer(
+              m_scratchBuffer, op.scratchBufferOffset, op.subFile->getSize(),
+              m_stagingBuffer, op.stagingBufferOffset, op.subFile->getCompressedSize());
+
+            scratchOffset += op.scratchBufferSize;
+            commandCount += 1;
+          }
+
+          // If any scratch memory is used, this means that compression
+          // commands have been recorded and we need a barrier.
+          if (scratchOffset) {
+            context->memoryBarrier(
+              GfxUsage::eDecompressionDst, 0,
+              GfxUsage::eTransferSrc, 0);
+          }
+
+          // Copy data from the staging or scratch buffer to the images
+          for (size_t i = firstCommand; i < firstCommand + commandCount; i++) {
+            const auto& op = ops[i];
+
+            if (op.type != GfxTransferOpType::eUploadImage)
+              continue;
+
+            bool scratch = useGpuDecompression(op.subFile);
+
+            Extent3D extent = op.dstImage->computeMipExtent(op.dstImageSubresources.mipIndex);
+
+            context->copyBufferToImage(op.dstImage,
+              op.dstImageSubresources, Offset3D(0, 0, 0), extent,
+              scratch ? m_scratchBuffer : m_stagingBuffer,
+              scratch ? op.scratchBufferOffset : op.stagingBufferOffset,
+              Extent2D(extent));
+          }
+
+          firstCommand += commandCount;
+        }
+
+        // Issue a final memory barrier to make transfer commands visible
+        context->memoryBarrier(GfxUsage::eTransferDst | GfxUsage::eDecompressionDst, 0, 0, 0);
+
+        // Prepare the command submission, and queue it for
+        // execution when the I/O request has completed.
+        request->executeOnCompletion([
+          cDevice       = m_device,
+          cCommandList  = context->endCommandList(),
+          cSemaphore    = m_semaphore,
+          cBatchId      = op.batchId
+        ] (IoStatus status) mutable {
+          if (status != IoStatus::eSuccess)
+            Log::err("GfxTransferManager: An I/O error has occured on batch ", cBatchId);
+
+          GfxCommandSubmission submission;
+          submission.addCommandList(GfxCommandList(cCommandList));
+          submission.addSignalSemaphore(cSemaphore, cBatchId);
+
+          cDevice->submit(GfxQueue::eComputeTransfer, std::move(submission));
+        });
+
+        // Submit retire operation to the completion thread.
+        lock.lock();
+
+        GfxTransferOp retireOp;
+        retireOp.type = GfxTransferOpType::eRetire;
+        retireOp.batchId = op.batchId;
+        retireOp.stagingBufferOffset = stagingBufferOffset;
+        retireOp.stagingBufferSize = stagingBufferSize;
+        retireOp.scratchBuffer = m_scratchBuffer;
+
+        m_completionQueue.push(std::move(retireOp));
+        m_completionCond.notify_one();
+
+        ops.clear();
+      }
+    }
+  }
+}
+
+
+void GfxTransferManagerIface::retire() {
+  while (true) {
+    std::unique_lock lock(m_mutex);
+
+    m_completionCond.wait(lock, [this] {
+      return !m_completionQueue.empty();
+    });
+
+    // Note that this operation may hold a reference to the
+    // scratch buffer, which we must keep alive for now
+    GfxTransferOp op = std::move(m_completionQueue.front());
+    m_completionQueue.pop();
+
+    // Exit wrker thread if requested. We can do this early
+    // since stop requests have no payload attached.
+    if (op.type == GfxTransferOpType::eStop)
+      return;
+
+    // We shouldn't really get anything other than retire ops
+    if (op.type != GfxTransferOpType::eRetire)
+      continue;
+
+    // Unlock so that we can wait for the GPU
+    lock.unlock();
+
+    m_semaphore->wait(op.batchId);
+
+    // Re-acquire lock and free the staging buffer region
+    // attached to this this operation.
+    lock.lock();
+
+    if (op.stagingBufferSize)
+      m_stagingAllocator.free(op.stagingBufferOffset, op.stagingBufferSize);
+
+    m_retireCond.notify_one();
+  }
+}
+
+
+uint64_t GfxTransferManagerIface::computeAlignedSize(
+  const IoArchiveSubFile*             subFile) const {
+  return useGpuDecompression(subFile)
+    ? align<uint64_t>(subFile->getCompressedSize(), 64)
+    : align<uint64_t>(subFile->getSize(), 64);
+}
+
+
+bool GfxTransferManagerIface::useGpuDecompression(
+  const IoArchiveSubFile*             subFile) const {
+  return m_gpuDecompression && subFile->getCompressionType() == IoArchiveCompression::eGDeflate;
+}
+
+
+bool GfxTransferManagerIface::useDirectUpload(
+  const GfxTransferOp&                op) const {
+  return (op.type == GfxTransferOpType::eUploadBuffer)
+      && (op.dstBuffer->getDesc().usage & GfxUsage::eCpuWrite)
+      && (!useGpuDecompression(op.subFile));
+}
+
+}
---
+++ src/gfx/gfx_transfer.h
@@ -0,0 +1,267 @@
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+#include "../alloc/alloc_chunk.h"
+
+#include "../io/io_archive.h"
+
+#include "gfx_device.h"
+
+namespace as {
+
+/**
+ * \brief Transfer operation type
+ *
+ * Used by the internal worker to determine
+ * what to do with a given transfer command.
+ */
+enum class GfxTransferOpType : uint32_t {
+  /** No operation */
+  eNone,
+  /** Buffer upload */
+  eUploadBuffer,
+  /** Image upload */
+  eUploadImage,
+  /** Flush current batch */
+  eFlush,
+  /** Retires batch */
+  eRetire,
+  /** Stop worker threads */
+  eStop,
+};
+
+
+/**
+ * \brief Tansfer operation
+ */
+struct GfxTransferOp {
+  /** Archive sub-file to read from */
+  const IoArchiveSubFile* subFile = nullptr;
+  /** Transfer batch ID */
+  uint64_t batchId = 0;
+  /** Allocated staging buffer range */
+  uint64_t stagingBufferOffset = 0;
+  uint64_t stagingBufferSize = 0;
+  /** Destination buffer for buffer uploads */
+  GfxBuffer dstBuffer;
+  uint64_t dstBufferOffset = 0;
+  /** Scratch buffer reference for retriement */
+  GfxBuffer scratchBuffer;
+  uint64_t scratchBufferOffset = 0;
+  uint64_t scratchBufferSize = 0;
+  /** Destination image for image uploads */
+  GfxImage dstImage;
+  GfxImageSubresource dstImageSubresources;
+  /** Transfer operation */
+  GfxTransferOpType type = GfxTransferOpType::eNone;
+};
+
+
+/**
+ * \brief Asynchronous transfer manager
+ *
+ * Implements transfers on top of the compute upload queue,
+ * integrating with I/O archives in order to transparently
+ * perform decompression as necessary.
+ *
+ * Internally, this will hold a large system memory staging
+ * buffer, which effectively throttles transfers in case of
+ * a bottleneck.
+ *
+ * As for the execution model, transfers will execute and
+ * complete in the order they are submitted. This may in
+ * some cases reduce efficiency, but makes synchronization
+ * with transfers significantly more convenient since only
+ * the batch ID from the last submission needs to be
+ * remembered.
+ *
+ * All methods in this class are thread-safe, however no
+ * lifetime management is performed. All objects involved
+ * in a transfer operation \e must be kept alive until the
+ * transfer has completed.
+ */
+class GfxTransferManagerIface {
+  constexpr static size_t ContextCount = 4;
+public:
+
+  /**
+   * \brief Initializes transfer manager
+   *
+   * \param [in] io I/O subsystem instance
+   * \param [in] device Graphics device
+   * \param [in] stagingBufferSize Size of the system memory
+   *    buffer, in bytes. No single transfer operation may
+   *    be larger than the staging buffer size.
+   */
+  GfxTransferManagerIface(
+          Io                            io,
+          GfxDevice                     device,
+          uint64_t                      stagingBufferSize);
+
+  /**
+   * \brief Destroys transfer manager
+   *
+   * Waits for all pending transfers to complete.
+   */
+  ~GfxTransferManagerIface();
+
+  /**
+   * \brief Enqueues a buffer upload
+   *
+   * All buffer data in the sub-file will be copied to the
+   * destination buffer at the given offset. If the source
+   * file is compressed, the buffer must have been created
+   * with \c GfxUsage::eDecompressionDst.
+   *
+   * If the buffer allows CPU write access and the source
+   * data is not compressed, no staging memory will be used
+   * as the buffer can be written to directly.
+   * \param [in] subFile Archive sub file containing the data
+   * \param [in] buffer Destination buffer
+   * \param [in] offset Destination buffer offset
+   * \returns Transfer batch ID for synchronization
+   */
+  uint64_t uploadBuffer(
+    const IoArchiveSubFile*             subFile,
+          GfxBuffer                     buffer,
+          uint64_t                      offset);
+
+  /**
+   * \brief Enqueues a texture upload
+   *
+   * Only full subresource uploads can be performed, so the
+   * destination image \e must be sized appropriately. When
+   * uploading multiple mip levels at once, subresource data
+   * must be tightly packed.
+   *
+   * In order to avoid queue ownership issues, the image should
+   * be created with \c GfxImageFlag::eSimultaneousAccess. Only
+   * a simple barrier invalidating caches will be needed on the
+   * queues using the image in that case.
+   * \param [in] subFile Archive sub file containing the data
+   * \param [in] image Destination image
+   * \param [in] subresources Destination subresources
+   * \returns Transfer batch ID for synchronization
+   */
+  uint64_t uploadImage(
+    const IoArchiveSubFile*             subFile,
+          GfxImage                      image,
+    const GfxImageSubresource&          subresources);
+
+  /**
+   * \brief Flushes current transfer batch
+   *
+   * This generally does not have to be called since polling the
+   * batch ID every frame will implicitly flush, however doing so
+   * may be useful if per-resource batch ID tracking is not desired.
+   *
+   * No operation will be performed if no transfer is queued up.
+   * \returns ID of the submitted transfer batch. This is always
+   *    equal to the batch ID of the last enqueued transfer operation.
+   */
+  uint64_t flush();
+
+  /**
+   * \brief Retrieves last completed batch ID
+   *
+   * This is the preferred way of synchronizing with pending
+   * transfers. All resources that were uploaded with a batch
+   * ID less than or equal to the last completed batch ID can
+   * safely be used.
+   *
+   * This may flush the current batch in order to guarantee
+   * forward progress.
+   * \returns \c true if the batch has completed
+   */
+  uint64_t getCompletedBatchId();
+
+  /**
+   * \brief Waits for a given transfer batch to complete
+   *
+   * This should be used sparingly, e.g. when loading a minimal
+   * set of resources at application startup without which the
+   * application cannot run in any meaningful way, such as UI
+   * textures and font resources.
+   * \param [in] batch ID of the transfer batch to wait for
+   */
+  void waitForCompletion(
+          uint64_t                      batch);
+
+private:
+
+  Io                                m_io;
+  GfxDevice                         m_device;
+  bool                              m_gpuDecompression;
+
+  ChunkAllocator<uint64_t>          m_stagingAllocator;
+  GfxBuffer                         m_stagingBuffer;
+  GfxBuffer                         m_scratchBuffer;
+
+  GfxSemaphore                      m_semaphore;
+
+  std::mutex                        m_mutex;
+
+  uint64_t                          m_batchId = 1;
+  uint64_t                          m_batchSize = 0;
+
+  std::array<GfxContext, ContextCount> m_contexts;
+
+  std::condition_variable           m_retireCond;
+  std::condition_variable           m_submissionCond;
+  std::queue<GfxTransferOp>         m_submissionQueue;
+  std::thread                       m_submissionThread;
+
+  std::condition_variable           m_completionCond;
+  std::queue<GfxTransferOp>         m_completionQueue;
+  std::thread                       m_completionThread;
+
+  uint64_t flushLocked();
+
+  uint64_t enqueueLocked(
+          GfxTransferOp&&               op);
+
+  GfxContext acquireContext(
+          uint64_t                      batchId);
+
+  void submit();
+
+  void retire();
+
+  uint64_t computeAlignedSize(
+    const IoArchiveSubFile*             subFile) const;
+
+  bool useGpuDecompression(
+    const IoArchiveSubFile*             subFile) const;
+
+  bool useDirectUpload(
+    const GfxTransferOp&                op) const;
+
+};
+
+
+/**
+ * \brief Transfer manager object
+ *
+ * See GfxTransferManagerIface.
+ */
+class GfxTransferManager : public IfaceRef<GfxTransferManagerIface> {
+
+public:
+
+  GfxTransferManager() { }
+  GfxTransferManager(std::nullptr_t) { }
+
+  GfxTransferManager(
+          Io                            io,
+          GfxDevice                     device,
+          uint64_t                      stagingBufferSize)
+  : IfaceRef<GfxTransferManagerIface>(std::make_shared<GfxTransferManagerIface>(
+      std::move(io), std::move(device), stagingBufferSize)) { }
+
+};
+
+}
