1a6fe24d1fd2a47fbb47b186af189126d96356eb
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2023-08-11 20:06:41 +0200

    util: Make handle type a signed integer.
    And default to -1 for undefined.

--- src/util/util_handle.h
+++ src/util/util_handle.h
@@ -16,29 +16,29 @@ class Handle {
 
 public:
 
-  Handle() { }
+  Handle() = default;
 
-  explicit Handle(uint32_t raw)
+  explicit Handle(int32_t raw)
   : m_raw(raw) { }
 
   /**
    * \brief Retrieves raw handle value
    * \returns Raw handle value
    */
-  explicit operator uint32_t () const {
+  explicit operator int32_t () const {
     return m_raw;
   }
 
-  bool operator == (const Handle& other) const { return m_raw == other.m_raw; }
-  bool operator != (const Handle& other) const { return m_raw != other.m_raw; }
+  bool operator == (const Handle&) const = default;
+  bool operator != (const Handle&) const = default;
 
   operator bool () const {
-    return m_raw != 0u;
+    return m_raw >= 0;
   }
 
 private:
 
-  uint32_t m_raw = 0u;
+  int32_t m_raw = -1;
 
 };
 
@@ -48,7 +48,7 @@ private:
 struct HandleHash {
   template<typename T>
   size_t operator () (const Handle<T>& handle) const {
-    return size_t(handle.getRaw());
+    return size_t(int32_t(handle));
   }
 };
 
