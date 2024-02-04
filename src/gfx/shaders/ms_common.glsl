caf8282670d6c3e74ef417ea00a03c0c9ee954e6
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2024-02-04 01:42:55 +0100

    gfx/shaders: Let mesh shader run with a variable export size chosen at runtime.

--- src/gfx/shaders/ms_common.glsl
+++ src/gfx/shaders/ms_common.glsl
@@ -5,16 +5,6 @@
 // size for us, depending on driver preferences.
 layout(local_size_x_id = SPEC_CONST_ID_MESH_SHADER_WORKGROUP_SIZE) in;
 
-// Restrict output to 128 vertices and primitives
-// by default, unless overridden by the application.
-#ifndef MS_MAX_VERT_COUNT
-#define MS_MAX_VERT_COUNT (128)
-#endif
-
-#ifndef MS_MAX_PRIM_COUNT
-#define MS_MAX_PRIM_COUNT (128)
-#endif
-
 // Convenience macro to perform count invocations of
 // subsequent code with arbitrary workgroup sizes.
 #define MS_LOOP_WORKGROUP(var, cond, count)                         \
